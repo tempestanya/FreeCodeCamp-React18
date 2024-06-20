90ebd5cbbac5c29f1f02289b47a53cdb23e72c6d
Anjali Thakur <1003anjali1003@gmail.com>
2024-06-20 23:00:07 +0530

    added 3 books

--- src/index.js
+++ src/index.js
@@ -3,23 +3,40 @@ import ReactDOM from 'react-dom/client';
 
 import "./index.css";
 
+const firstbook = {
+    author: 'Joseph Murphy ',
+    title: 'The Power of Your Subconscious Mind',
+    img: "https://m.media-amazon.com/images/I/51QnuLIY2uL._SY445_SX342_.jpg"
+};
+const secondbook = {
+    author: 'Francesc Miralles',
+    title: 'Ikigai',
+    img: "https://m.media-amazon.com/images/I/81l3rZK4lnL._SY342_.jpg"
+};
+const thirdbook = {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: "https://m.media-amazon.com/images/I/81Ls+SBCLiL._SY466_.jpg"
+};
+
+
 function BookList() {
     return(
         <section className='booklist'>
-            <Book />
-            <Book />
-            <Book />
-            <Book />
+            <Book author = {firstbook.author} title = {firstbook.title} img = {firstbook.img}/>
+            <Book author = {secondbook.author} title = {secondbook.title} img = {secondbook.img}/>
+            <Book author = {thirdbook.author} title = {thirdbook.title} img = {thirdbook.img}/>
         </section>
     )
 }
 
-const Book = () => {
+
+const Book = ({img, title, author} ) => {
     return(
         <article className='book'>
-            <img src="https://images-eu.ssl-images-amazon.com/images/I/619U3eefi7L._AC_UL600_SR600,400_.jpg" alt="Lucifer was Innocent" />
-            <h2>Lucifer was Innocent</h2>
-            <h4>Tirth Raj Parsana</h4>
+            <img src= {img} alt= {title} />
+            <h2>{title}</h2>
+            <h4>{author}</h4>
         </article>
     )
 }    

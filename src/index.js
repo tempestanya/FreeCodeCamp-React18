ca1d295c651d4b907c17ead9e1e23c62456099b9
Anjali Thakur <1003anjali1003@gmail.com>
2024-06-24 20:39:40 +0530

    Completed Amazon Best Sellers

--- src/index.js
+++ src/index.js
@@ -2,51 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import "./index.css";
-
-const books = [
-    {
-        author: 'Joseph Murphy ',
-        title: 'The Power of Your Subconscious Mind',
-        img: "https://m.media-amazon.com/images/I/51QnuLIY2uL._SY445_SX342_.jpg",
-        id: 1,
-    },
-    {
-        author: 'Francesc Miralles',
-        title: 'Ikigai',
-        img: "https://m.media-amazon.com/images/I/81l3rZK4lnL._SY342_.jpg",
-        id: 2,
-    },
-    {
-        author: 'James Clear',
-        title: 'Atomic Habits',
-        img: "https://m.media-amazon.com/images/I/81Ls+SBCLiL._SY466_.jpg",
-        id: 3,
-    }
-];
-
+import {books} from './books';
+import Book from './Book';
 
 function BookList() {
     return(
-        <section className='booklist'>
-            {books.map((book) => {
-                return <Book {...book} key = {book.id} />;
-            })}
-        </section>
+        <>
+            <h1>Amazon Best Sellers</h1>
+            <section className='booklist'>
+                {books.map((book, index) => {
+                    return <Book {...book} key = {book.id} number = {index} />;
+                })}
+            </section>
+        </>
     )
-}
-
-
-const Book = (props) => {
-    const { img, title, author } = props;
-    console.log(props);
-    return (
-        <article className='book'>
-            <img src= {img} alt= {title} />
-            <h2>{title}</h2>
-            <h4>{author}</h4>
-        </article>
-    )
-}    
+}   
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

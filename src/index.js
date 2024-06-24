ce22cd3e4455fe9dc3c19ae62d6bf5deb02f8c27
Anjali Thakur <1003anjali1003@gmail.com>
2024-06-24 17:29:24 +0530

    spread op, added readme (kush)

--- src/index.js
+++ src/index.js
@@ -3,36 +3,43 @@ import ReactDOM from 'react-dom/client';
 
 import "./index.css";
 
-const firstbook = {
-    author: 'Joseph Murphy ',
-    title: 'The Power of Your Subconscious Mind',
-    img: "https://m.media-amazon.com/images/I/51QnuLIY2uL._SY445_SX342_.jpg"
-};
-const secondbook = {
-    author: 'Francesc Miralles',
-    title: 'Ikigai',
-    img: "https://m.media-amazon.com/images/I/81l3rZK4lnL._SY342_.jpg"
-};
-const thirdbook = {
-    author: 'James Clear',
-    title: 'Atomic Habits',
-    img: "https://m.media-amazon.com/images/I/81Ls+SBCLiL._SY466_.jpg"
-};
+const books = [
+    {
+        author: 'Joseph Murphy ',
+        title: 'The Power of Your Subconscious Mind',
+        img: "https://m.media-amazon.com/images/I/51QnuLIY2uL._SY445_SX342_.jpg",
+        id: 1,
+    },
+    {
+        author: 'Francesc Miralles',
+        title: 'Ikigai',
+        img: "https://m.media-amazon.com/images/I/81l3rZK4lnL._SY342_.jpg",
+        id: 2,
+    },
+    {
+        author: 'James Clear',
+        title: 'Atomic Habits',
+        img: "https://m.media-amazon.com/images/I/81Ls+SBCLiL._SY466_.jpg",
+        id: 3,
+    }
+];
 
 
 function BookList() {
     return(
         <section className='booklist'>
-            <Book author = {firstbook.author} title = {firstbook.title} img = {firstbook.img}/>
-            <Book author = {secondbook.author} title = {secondbook.title} img = {secondbook.img}/>
-            <Book author = {thirdbook.author} title = {thirdbook.title} img = {thirdbook.img}/>
+            {books.map((book) => {
+                return <Book {...book} key = {book.id} />;
+            })}
         </section>
     )
 }
 
 
-const Book = ({img, title, author} ) => {
-    return(
+const Book = (props) => {
+    const { img, title, author } = props;
+    console.log(props);
+    return (
         <article className='book'>
             <img src= {img} alt= {title} />
             <h2>{title}</h2>
